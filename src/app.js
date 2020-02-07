7fc8642ef724114dc1cec253a93bc681c5e786b2
Felipe Vieira <felipe.svfx@gmail.com>
2020-02-06 23:41:40 -0200

    Tratativa de erros/Sentry

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
-/*
-    Estrutura da aplicação;
-*/
+/**
+ *  Estrutura da aplicação;
+ */
 import express from 'express';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
 import path from 'path';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 import './database/index';
 
-/*
-    Definição da classe do app;
-*/
 class App {
   // Executado automaticamente quando a classe 'App' é chamada;
   constructor() {
     this.server = express(); // variável de declaração de rotas.
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   /*
-      Permite enviar e receber requisições do tipo JSON;
+      Permite enviar e receber requisições do tipo JSON
   */
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -30,11 +36,17 @@ class App {
 
   routes() {
     this.server.use(routes); // Rotas importadas do routes.js.
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
-/*
-    Exportação do App direto para o server;
-*/
-
+// Exportação do App para o server
 export default new App().server;
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://ba5932c1424e4cde8252813e2f47188b@sentry.io/2309797',
+};
